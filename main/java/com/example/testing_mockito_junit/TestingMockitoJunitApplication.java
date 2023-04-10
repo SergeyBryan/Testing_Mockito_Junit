5520d3df2dcfe5c05857ebadae74ca4fff50d227
sergeybryan <regkmastis@yandex.ru>
2023-04-10 13:22:44 +0300

    Initial commit

---
+++ main/java/com/example/testing_mockito_junit/TestingMockitoJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing_mockito_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMockitoJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingMockitoJunitApplication.class, args);
+    }
+
+}
---
+++ test/java/com/example/testing_mockito_junit/TestingMockitoJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_mockito_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingMockitoJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
