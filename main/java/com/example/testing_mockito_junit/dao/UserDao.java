5d0def56cd73b667447e97007ea2261f2392716a
sergeybryan <regkmastis@yandex.ru>
2023-04-10 13:34:07 +0300

    Домашняя работа по Mockito

---
+++ main/java/com/example/testing_mockito_junit/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.example.testing_mockito_junit.dao;
+
+import com.example.testing_mockito_junit.model.User;
+
+import java.util.Set;
+
+public interface UserDao {
+
+
+    User getUserByName(String name);
+
+    Set<User> findAllUsers();
+}
